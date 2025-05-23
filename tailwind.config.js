6a549f3aba84357e3662b6af164f964250c595b1
Strayyyy <jakobternel@gmail.com>
2025-05-23 15:46:37 +1000

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+        fontFamily: {
+            main: ["Inter", "sans-serif"],
+            heading: ["Space Grotesk", "sans-serif"]
+        },
+        colors: {
+            background: "#0D1B2A",
+            primary: "#2563EB",
+            secondary: "#1E293B",
+            outline: "#334155",
+
+            light: "#F1F5F9",
+            muted: "#94A3B8",
+
+            lime: "#A3E635",
+            pink: "#F472B6",
+            cyan: "#06B6D4"
+        }
+    },
+  },
+  plugins: [],
+}
