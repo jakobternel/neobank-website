7ca2be2e1c8b05e554038a40dfb2a4c47148a36d
Strayyyy <jakobternel@gmail.com>
2025-06-02 12:23:45 +1000

    added crypto component to page

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import { useEffect } from "react";
 import Navigation from "./components/Navigation";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
+import InternationalBenefits from "./components/InternationalBenefits";
+import Crypto from "./components/Crypto";
 import Footer from "./components/Footer";
 
 import { useAppSelector } from "./store/hooks";
-import InternationalBenefits from "./components/InternationalBenefits";
 
 function App() {
     const theme = useAppSelector((state) => state.theme.mode);
@@ -17,11 +18,13 @@ function App() {
 
     return (
         <div>
+            {/*  className="breakpointDev" */}
             <Navigation />
             <div className="flex flex-col gap-32">
                 <Hero />
                 <Features />
                 <InternationalBenefits />
+                <Crypto />
                 <Footer />
             </div>
         </div>
